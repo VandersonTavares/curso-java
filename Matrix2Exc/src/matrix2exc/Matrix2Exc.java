6435f5e19be27c0bc5f51df9fe68d850d8dfa91a
Vanderson Tavares da Silva <vandersontavares32@gmail.com>
2021-09-29 13:16:23 -0300

    .gitignore

---
+++ Matrix2Exc/src/matrix2exc/Matrix2Exc.java
@@ -0,0 +1,13 @@
+/*Fazer um programa para ler dois numeros inteiros M e N, 
+e depois ler uma matriz de M linhas por N colunas contendo numeros inteiros, 
+podendo haver repetições. Em seguida, ler um numero inteiro x que pertence à matriz. 
+Para cada ocorrência de X, mostrar os valores à esquerda, acima, à direita e abaixo de X, 
+quando houver, conforme exemplo.*/
+
+package matrix2exc;
+public class Matrix2Exc {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
